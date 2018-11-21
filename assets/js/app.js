4a4d458c53ab81584a3a3900d338f4171334c6aa
RyanJF <ryanjfreeman77@gmail.com>
2018-11-20 18:53:30 -0600

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ const player = document.getElementById('player');
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 const captureButton = document.getElementById('capture');
+const imgElement = document.getElementById('myImage');
 
 const constraints = {
     video: {
@@ -11,9 +12,17 @@ const constraints = {
 
 };
 
+
 captureButton.addEventListener('click', () => {
     // Draw the video frame to the canvas.
     context.drawImage(player, 0, 0, canvas.width, canvas.height);
+    // Stop all video streams.
+    player.srcObject.getVideoTracks().forEach(track => track.stop());
+    // let dataURL = canvas.toDataURL('image/png');
+    let dataURL = canvas.toDataURL('image/png');
+    // .replace(/^data:image\/(png|jpg);base64,/, '');
+    console.log(dataURL);
+    imgElement.setAttribute('src', dataURL);
 });
 
 // Attach the video stream to the video element and autoplay.
